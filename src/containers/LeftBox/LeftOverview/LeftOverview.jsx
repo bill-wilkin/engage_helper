e7626156d8f136720e6ade83be2167838519c095
Spencer Attick <spencer.attick@gmail.com>
2024-08-28 09:44:45 -0400

    add LeftBox folders/files

---
+++ src/containers/LeftBox/LeftOverview/LeftOverview.jsx
@@ -0,0 +1,13 @@
+import Description from '../Description/Description';
+import MappingInstructions from '../MappingInstructions/MappingInstructions';
+
+const LeftOverview = () => {
+    return (
+        <>
+            <Description/>
+            <MappingInstructions/>
+        </>
+    )
+}
+
+export default LeftOverview;
