e83a4901b394a1d846d1a75db61c68935ab788a6
Ashton Huxtable <ashton.huxtable@segment.com>
2024-08-30 15:08:15 -0600

    Update coopy to reflect success

--- src/containers/RightBox/SamplePayload/CopyPayload.jsx
+++ src/containers/RightBox/SamplePayload/CopyPayload.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { CopyIcon } from "./Icon";
+import { useState } from "react";
+import { CopyIcon, CopySuccessIcon } from "./Icon";
 import './SamplePayload.css'
 
 const CopyPayload = ({ textToCopy }) => {
+    const [copySuccess, setCopySuccess] = useState(false)
+
+    const successfulCopy = () => {
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 1000); 
+    }
+
     const copyToClipboard = () => {
         const textArea = document.createElement('textarea');
         textArea.value = textToCopy;
@@ -10,11 +18,19 @@ const CopyPayload = ({ textToCopy }) => {
         textArea.select();
         document.execCommand('copy');
         document.body.removeChild(textArea);
+        successfulCopy();
     }
   
-    return <button className='copy-button' onClick={copyToClipboard}>
-        <CopyIcon />
-    </button>;
+    return (
+        <div className="copy-success-container">
+          {copySuccess ?  
+            <div className="copy-success"> <CopySuccessIcon /> </div> :
+            <button className="copy-button" onClick={copyToClipboard}>
+              <CopyIcon /> 
+            </button> 
+          }
+        </div>
+    )
   };
   
   export default CopyPayload;
